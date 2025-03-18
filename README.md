8ef7d614b992c0a807b857010be3abf36a3a49ae
Andres Gomez Casanova - AngocA <angoca@yahoo.com>
2025-03-17 23:38:35 -0500

    Version funcional, falta interfaz

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
-# MiNegocio de MaptimeBogota
+# MiColegio en OSM Colombia
 
-MiNeogcio es un fork de OnOSM.org enfocado a la ciudad Bogotá.
-Esta página ayuda a propietarios o administradores de negocios a indicar los datos relativos a sus establecimientos.
-El resultado es escrito en una [Nota de OpenStreetMap](https://wiki.openstreetmap.org/wiki/ES:Notas) de forma anónima.
+MiColegio es un fork de OnOSM.org enfocado al mapeo de colegios y escuelas en Colombia.
+Esta página ayuda a profesores y estudiantes a describir sus centro educativos para que posteriormente puedan ser mapeados correctamente en OpenStreetMap.
+
+El proceso consiste en llenar un formulario y el resultado es escrito en una [Nota de OpenStreetMap](https://wiki.openstreetmap.org/wiki/ES:Notas) de forma anónima.
 
 Para ver las notas creadas recientemente se puede consultar la [página NotesReview](https://ent8r.github.io/NotesReview/?view=map&map=11%2F4.6552%2F-74.1111&query=MaptimeBogota&status=open) donde se resaltan las notas creadas por este mecanismo.
-Una vez identificada una nota, esta se puede comentar para indicar más información.
-Las notas serán procesadas por los contribuidores del mapa, quienes convertirán la información en un cambio en el mapa, donde se podrán ver los datos del establecimiento.
+Una vez identificada una nota, esta se puede comentar para indicar más información sobre el establecimiento educativo.
 
-Esto hace parte de la iniciativa de [resolución de notas en Colombia](https://wiki.openstreetmap.org/wiki/ES:Colombia/Resoluci%C3%B3n_de_notas), liderada por MaptimeBogota.
+Las notas serán procesadas por los contribuidores del mapa, quienes convertirán la información incluida en la nota en un cambio en el mapa. Cuando se hagan los cambios del mapa, se podrán ver los datos del colegio o escuela directamente en el mapa.
 
-Se hizo un fork de OnOSM.org para poder hacer algo más adaptado a las condiciones de Colombia, especialmente las de Bogotá.
-El proceso de registro de un negocio se explica de acuerdo a la información necesaria para la ciudad de Bogotá, usando la terminología colombiana.
+Se hizo un fork de OnOSM.org para poder hacer algo más adaptado a las condiciones de Colombia.
+
+El formulario de esta página se basa en la siguiente documentación de OSM:
+
+* [Cómo mapear un...](https://wiki.openstreetmap.org/wiki/User:Angoca/Como_mapear_un).
+* [Guía para mapear instituciones educativas](https://wiki.openstreetmap.org/wiki/ES:Colombia/Gu%C3%ADa_para_mapear/sitios_educativos).
--- js/site.js
+++ js/site.js
@@ -1,45 +1,51 @@
 /*
-Este archivo JavaScript permite modfiicar la apareciencia de la página HTML, mostrando y ocultando elementos.
+Este archivo JavaScript permite modificar la apareciencia de la página HTML,
+mostrando y ocultando elementos.
 También permite hacer algunas pocas validaciones sobre los datos datos.
 Igualmente, maneja el mapa por medio de LeafLet.
 
-Version 2021-10-30
+Version 2025-03-17
 */
+
+/**
+ * Crea un objeto de tipo mapa con la librería Leaflet.
+ */
 var findme_map = L.map('findme-map')
-    .setView([4.65341, -74.08363], 12),
+    .setView([4, -72.7], 6),
     osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    osmAttrib = '© OpenStreetMap contributors. <a href="http://www.openstreetmap.org/copyright">License</a>',
-    osm = L.tileLayer(osmUrl, {minZoom: 9, maxZoom: 19, attribution: osmAttrib}).addTo(findme_map),
+    osmAttrib = '© Contribuidores de OpenStreetMap. <a href="http://www.openstreetmap.org/copyright">Licencia</a>',
+    osm = L.tileLayer(osmUrl, { minZoom: 6, maxZoom: 20, attribution: osmAttrib }).addTo(findme_map),
     category_data = [];
 
-var findme_marker = L.marker([0,0], {draggable:true}).addTo(findme_map);
+var findme_marker = L.marker([0, 0], { draggable: true }).addTo(findme_map);
 findme_marker.setOpacity(1);
 
 if (location.hash) location.hash = '';
 
+/**
+ * Hace zoom al punto definido y muestra una chincheta.
+ * @param {*} chosen_place Ubicación deseada.
+ * @param {*} map Mapa donde se debe mostrar.
+ * @param {*} marker Chincheta.
+ */
 function zoom_to_point(chosen_place, map, marker) {
+    // Imprime en consola el lugar escogido.
     console.log(chosen_place);
 
     marker.setOpacity(1);
     marker.setLatLng([chosen_place.lat, chosen_place.lon]);
 
-
-    map.setView(chosen_place, 18, {animate: true});
+    map.setView(chosen_place, 13, { animate: true });
 }
 
-
-$.getJSON('./categories.json', function(data) {
-        $("#category").select2({
-            multiple: true,
-            data: data,
-        })})
-        ;
-
+/**
+ * Procesa la ubicación gracias al mecanismo de ubicación del teléfono (GPS).
+ */
 $("#use_my_location").click(function (e) {
     $("#couldnt-find").hide();
     $("#success").hide();
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var point = {
                 lat: position.coords.latitude,
                 lon: position.coords.longitude
@@ -47,24 +53,27 @@ $("#use_my_location").click(function (e) {
 
             zoom_to_point(point, findme_map, findme_marker);
 
-            $('#success').html("<strong>¡Encontrado!</strong> Mueve la chincheta hasta que esté <strong>exactamente sobre la ubicación de tu negocio</strong>, preferiblemente en la mitad del establecimiento.<br />Te recomendamos que hagas bastante zoom para ubicar la chincheta correctamente.<br />Cuando la hayas ubicado bien, puedes pasar a la siguiente sección:<br /> <a href='javascript:check_coordinates()'>Paso 2: Agregar información del establecimiento</a>.");
+            $('#success').html("<strong>¡Encontrado!</strong> Mueve la chincheta hasta que esté <strong>ubicado exactamente sobre tu colegio o escuela</strong>, preferiblemente en la mitad del establecimiento educativo.<br />Te recomendamos que hagas bastante zoom para ubicar la chincheta correctamente.<br />Cuando la hayas ubicado bien, puedes pasar a la siguiente sección:<br /> <a href='javascript:check_coordinates()'><strong>Paso 2:</strong> Agregar información del establecimiento educativo</a>.");
             $('#success').show();
-            window.scrollTo(0, $('#address').position().top - 30);
+            window.scrollTo(0, $('#addressToFind').position().top - 30);
             $('.step-2 a').attr('href', '#details');
         }, function (error) {
             $("#couldnt-find").show();
         });
     } else {
-      $("#couldnt-find").show();
+        $("#couldnt-find").show();
     }
 });
 
-$("#find").submit(function(e) {
+/**
+ * Procesa una dirección para obtener su ubicación.
+ */
+$("#find").submit(function (e) {
     e.preventDefault();
     $("#couldnt-find").hide();
     $("#invalid-location").hide();
     $("#success").hide();
-    var address_to_find = $("#address").val() + " Bogotá, Colombia";
+    var address_to_find = $("#addressToFind").val();
     if (address_to_find.length === 0) return;
     var qwarg = {
         format: 'json',
@@ -73,13 +82,13 @@ $("#find").submit(function(e) {
     var url = "https://nominatim.openstreetmap.org/search?" + $.param(qwarg);
     $("#findme h4").text('Buscando...');
     $("#findme").addClass("loading");
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function (data) {
         if (data.length > 0) {
             zoom_to_point(data[0], findme_map, findme_marker);
 
-            $('#success').html("<strong>¡Encontrado!</strong> Mueve la chincheta hasta que esté <strong>exactamente sobre la ubicación de tu negocio</strong>, preferiblemente en el centro del establecimiento.<br />Te recomendamos que hagas bastante zoom para ubicar la chincheta correctamente.<br />Cuando lo hayas ubicado bien, puedes pasar a la siguiente sección:<br /> <a href='javascript:check_coordinates()'>Paso 2: Agregar información del establecimiento</a>.");
+            $('#success').html("<strong>¡Encontrado!</strong> Mueve la chincheta hasta que esté <strong>ubicado exactamente sobre tu colegio o escuela</strong>, preferiblemente en el centro del establecimiento educativo.<br />Te recomendamos que hagas bastante zoom para ubicar la chincheta correctamente.<br />Cuando lo hayas ubicado bien, puedes pasar a la siguiente sección:<br /> <a href='javascript:check_coordinates()'><strong>Paso 2:</strong> Agregar información del establecimiento educativo</a>.");
             $('#success').show();
-            window.scrollTo(0, $('#address').position().top - 30);
+            window.scrollTo(0, $('#addressToFind').position().top - 30);
             $('.step-2 a').attr('href', '#details');
         } else {
             $("#couldnt-find").show();
@@ -88,38 +97,49 @@ $("#find").submit(function(e) {
     });
 });
 
-$(window).on('hashchange', function() {
+/**
+ * Hace los cambios de la página de acuerdo al paso en el que se esté.
+ * Lo reliza ocultando partes y activando otras.
+ */
+$(window).on('hashchange', function () {
+    // Paso para dar los detalles de la institución (intermedio).
     if (location.hash == '#details') {
-        $('#collect-data-step').removeClass('hide');
         $('#address-step').addClass('hide');
+        $('#collect-data-step').removeClass('hide');
         $('#confirm-step').addClass('hide');
         $('.steps').addClass('on-2');
         $('.steps').removeClass('on-3');
-    } else if (location.hash == '#done') {
-        $('#confirm-step').removeClass('hide');
-        $('#collect-data-step').addClass('hide');
-        $('#address-step').addClass('hide');
-        $('.steps').addClass('on-3');
-    } else {
-        $('#address-step').removeClass('hide');
-        $('#collect-data-step').addClass('hide');
-        $('#confirm-step').addClass('hide');
-        $('.steps').removeClass('on-2');
-        $('.steps').removeClass('on-3');
-    }
+    } else
+        // Para para crear la nota (final).
+        if (location.hash == '#done') {
+            $('#address-step').addClass('hide');
+            $('#collect-data-step').addClass('hide');
+            $('#confirm-step').removeClass('hide');
+            $('.steps').addClass('on-3');
+        } else
+        // Pasa definir la ubicaicón (inicial).
+        {
+            $('#address-step').removeClass('hide');
+            $('#collect-data-step').addClass('hide');
+            $('#confirm-step').addClass('hide');
+            $('.steps').removeClass('on-2');
+            $('.steps').removeClass('on-3');
+        }
     findme_map.invalidateSize();
 });
 
-$("#collect-data-done").click(function() {
+/**
+ * Toma los datos del formulario y genera una URL para crear una nota anónima
+ * en OSM. Comienza validando los datos del formulario, asegurándose que los
+ * datos obligatorios están diligenciados.
+ */
+$("#collect-data-done").click(function () {
     // Basic form validation
-    if ($("#category").val().length == 0) {
-        $("#form-invalid").text('Error: Selecciona al menos una categoría.');
+    if ($("#institutionName").val().length < 5) {
+        $("#form-invalid").text('Error: Ingresa el nombre completo del establecimiento (mínimo 5 caracateres).');
         return false;
-    } else if ($("#name").val().length < 2) {
-        $("#form-invalid").text('Error: Ingresa el nombre completo del establecimiento.');
-        return false;
-    } else if ($("#businessaddress").val().length < 9) {
-        $("#form-invalid").text('Error: Ingresa una dirección válida.');
+    } else if ($("#institutionAddress").val().length < 10) {
+        $("#form-invalid").text('Error: Ingresa una dirección válida (mínimo 10 caracteres).');
         return false;
     } else {
         $("#form-invalid").text("");
@@ -128,18 +148,82 @@ $("#collect-data-done").click(function() {
     location.hash = '#done';
 
     var note_body =
-        "Datos del establecimiento: \n" +
-        "Nombre: " + $("#name").val() + "\n" +
-        "Categorías: " + $("#category").val().join(", ") + "\n" +
-        "Dirección: " + $("#businessaddress").val() + "\n" +
-        "Teléfono: " + $("#phone").val() + "\n" +
-        "Horario: " + $("#opening_hours").val() + "\n" +
-        "Website: " + $("#website").val() + "\n" +
-        "Correo-e: " + $("#email").val() + "\n" +
-        "Twitter: " + $("#twitter").val() + "\n" +
-        "Facebook: " + $("#facebook").val() + "\n" +
-        "Notas: " + $("#notes").val() + "\n" +
-        "#MaptimeBogota https://maptimebogota.github.io/minegocio\n",
+        "Datos de la institución educativa: \n" +
+        "amenity=school\n" +
+        "name=" + $("#institutionName").val() + "\n" +
+        "address=" + $("#institutionAddress").val() + "\n";
+
+    if ($("#institutionPhone").val() == null) {
+        var note_body = note_body +
+            "phone=" + $("#institutionPhone").val() + "\n";
+    }
+    if ($("#website").val() == null) {
+        var note_body = note_body +
+            "website=" + $("#website").val() + "\n";
+    }
+    if ($("#operator_type").val() == null) {
+        var note_body = note_body +
+            "operator:type=" + $("#operator_type").val() + "\n";
+    }
+    if ($("#operator").val() == null) {
+        var note_body = note_body +
+            "operator=" + $("#operator").val() + "\n";
+    }
+    if ($("#min_age").val() == null) {
+        var note_body = note_body +
+            "min_age=" + $("#min_age").val() + "\n";
+    }
+    if ($("#max_age").val() == null) {
+        var note_body = note_body +
+            "max_age=" + $("#max_age").val() + "\n";
+    }
+    if ($("#service_times:ref:CO").val() == null) {
+        var note_body = note_body +
+            "service_times:ref:CO=" + $("#service_times:ref:CO").val() + "\n";
+    }
+    if ($("#religion").val() == null) {
+        var note_body = note_body +
+            "religion=" + $("#religion").val() + "\n";
+    }
+
+    if ($("#institutionEmail").val() == null) {
+        var note_body = note_body +
+            "email: " + $("#institutionEmail").val() + "\n";
+    }
+    if ($("#isced:level").val() == null) {
+        var note_body = note_body +
+            "isced:level=" + $("#isced:level").val() + "\n";
+    }
+    if ($("#gender").val() == null) {
+        var note_body = note_body +
+            "gender=" + $("#gender").val() + "\n";
+    }
+    if ($("#fee").val() == null) {
+        var note_body = note_body +
+            "fee=" + $("#fee").val() + "\n";
+    }
+    if ($("#language").val() == null) {
+        var note_body = note_body +
+            "language=" + $("#language").val() + "\n";
+    }
+    if ($("#wikipedia").val() == null) {
+        var note_body = note_body +
+            "wikipedia=" + $("#wikipedia").val() + "\n";
+    }
+    if ($("#wikidata").val() == null) {
+        var note_body = note_body +
+            "wikidata=" + $("#wikidata").val() + "\n";
+    }
+    if ($("#notes").val() == null) {
+        var note_body = note_body +
+            "Notas: " + $("#notes").val() + "\n";
+    }
+    var note_body = note_body +
+        "\n" +
+        "Revisar school=*.\n" +
+        "Adaptar grades=*.\n" +
+        "En el caso de religión, adaptar denomination=*.\n" +
+        "#OSM-Colombia https://osm-colombia.github.io/micolegio/\n",
         latlon = findme_marker.getLatLng(),
         note_data = {
             lat: latlon.lat,
@@ -147,10 +231,15 @@ $("#collect-data-done").click(function() {
             text: note_body
         };
 
+    // Imprime en consola el contenido de la nota.
+    console.log(note_body);
+
+
+    // Crea la nota por medio de un la generación de la URL.
     $.post(
         'https://api.openstreetmap.org/api/0.6/notes.json',
         note_data,
-        function(result) {
+        function (result) {
             var id = result.properties.id;
             $("#linkcoords").append(
                 '<a href="https://osm.org/note/' + id + '">https://osm.org/note/' + id + '</a>'
@@ -159,17 +248,38 @@ $("#collect-data-done").click(function() {
     );
 });
 
+/**
+ * Restablece los campos para insertar otro colegio. Limpia los campos que se
+ * habían diligenciado anteriormente.
+ */
 function clearFields() {
-    $("#name").val('');
-    $("#phone").val('');
+    $("#institutionName").val('');
+    $("#institutionPhone").val('');
     $("#website").val('');
-    $("#twitter").val('');
-    $("#opening_hours").val('');
-    $("#category").val('');
-    $("#businessaddress").val('');
+    $("#operator_type").val('');
+    $("#operator").val('');
+    $("#min_age").val('');
+    $("#max_age").val('');
+    $("#service_times:ref:CO").val('');
+    $("#religion").val('');
+
+    $("#institutionAddress").val('');
+    $("#institutionEmail").val('');
+    $("#isced:level").val('');
+    $("#gender").val('');
+    $("#fee").val('');
+    $("#language").val('');
+    $("#wikipedia").val('');
+    $("#wikidata").val('');
+
+    $("#notes").val('');
     $("#linkcoords").empty();
 }
 
+/**
+ * Chequea las coordenadas antes de pasar el siguiente paso. Si están bien,
+ * activa la sección de detalles; si está mal, muestra un mensaje de error.
+ */
 function check_coordinates() {
     var latlon = findme_marker.getLatLng();
 
